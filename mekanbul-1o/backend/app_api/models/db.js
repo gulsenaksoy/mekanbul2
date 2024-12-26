6fc41d6f48ef931c3070733edb4243c79a9f862a
gulsenaksoy <gulsenaksoyy32@gmail.com>
2024-12-26 13:01:10 +0300

    son

---
+++ mekanbul-1o/backend/app_api/models/db.js
@@ -0,0 +1,18 @@
+var mongoose=require('mongoose');
+//var dbURI="mongodb+srv://gulsenaksoyy32:gulsen123@cluster0.8xdyf.mongodb.net/mekanbul?retryWrites=true&w=majority&appName=Cluster0";
+
+var dbURI="mongodb://localhost/mekanbul";
+mongoose.connect(dbURI);
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+" adresine bağlandı");
+});
+mongoose.connection.on("disconnected",function(){
+    console.log(dbURI+" bağlantısı koptu");
+});
+
+process.on("SIGINT",function(){
+    mongoose.connection.close();
+    console.log("Bağlantı kapatıldı");
+    process.exit(0);
+});
+require("./venue");
